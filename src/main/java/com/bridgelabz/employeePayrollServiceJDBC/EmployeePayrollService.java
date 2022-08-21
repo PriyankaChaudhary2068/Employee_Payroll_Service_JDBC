0c19bfac4a72619089574645ea2397d00be02e69
chutki12 <priyankachaudhary2068>
2022-08-21 22:46:52 +0530

    ADDED : Updated Salary Data

--- src/main/java/com/bridgelabz/employeePayrollServiceJDBC/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeePayrollServiceJDBC/EmployeePayrollService.java
@@ -1,41 +1,84 @@
 package com.bridgelabz.employeePayrollServiceJDBC;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class EmployeePayrollService {
-
-	   public static void main(String[] args) throws SQLException {
-	        System.out.println("welcome to employee payroll data base connection ");
-
-	        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
-	        String userName = "root";
-	        String password = "vchutki2068vpc";
-	        Connection connection = null;
-
-	        try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            System.out.println("Driver loaded");
-	            connection = DriverManager.getConnection(jdbcUrl, userName, password);
-	            System.out.println("Connection done...");
-	            Statement statement = connection.createStatement();
-	            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
-	            while (resultSet.next()) {
-	                System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("name") + " | " + resultSet.getString("address") + " | " + resultSet.getString("department") + " | " + resultSet.getString("gender") + " | " + resultSet.getDate("start"));
-	            }
-	        } 
-	        catch (Exception e) 
-	        {
-	            e.printStackTrace();
-	        }
-	        finally
-	        {
-	            connection.close();
-	        }
-	    }
-	}
-
-	        
-
 	
+	
+		public static void connectToMysql() {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("Driver loaded");
+				Connection connection = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/payroll_service",
+						"root",
+						"vchutki2068vpc");
 
+				System.out.println("Successfully connected to MySQL database ");
+			} catch (SQLException e) {
+				System.out.println("An error occurred while connecting MySQL databse");
+				e.getMessage();
+			} catch (ClassNotFoundException e) {
+				System.out.println("cannot find the driver in the class path !");
+				e.getMessage();
+			}
+		}
+		private static Connection getConnection() throws SQLException {
+			Connection connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/payroll_service",
+					"root",
+					"vchutki2068vpc");
+			return connection;
+		}
+		public static void getAllDataUsingPreparedStatemnt() throws SQLException {
+			String select = "SELECT * FROM employee_payroll";
+			System.out.println(select);
+			Connection connection = null;
+			try {
+				connection = getConnection();
+				connection.setAutoCommit(false);
+				PreparedStatement statement = connection.prepareStatement(select);
+				ResultSet resultSet = statement.executeQuery();
+				while (resultSet.next()) {
+					String name = resultSet.getString("name");			
+					String salary = resultSet.getString("basicPay");
 
+					System.out.println("Data: "+ name + ", " + salary);
+				}
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				connection.rollback();
+			}
+		}
+		public static void updateDataUsingPreparedStatement(String basicPay, int id) throws SQLException {
+			System.out.println("\nUpdated Employee salary");
+			String update = "UPDATE employee_payroll SET basicPay=? where id=?";
+			System.out.println(update);
+			Connection connection = null;
+			try {
+				connection = getConnection();
+				connection.setAutoCommit(false);
+				PreparedStatement statement = connection.prepareStatement(update);
+				statement.setString(1,basicPay);
+				statement.setInt(2,id);
+				int rowEffected = statement.executeUpdate();
+				System.out.println(rowEffected + " record updated");
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				connection.rollback();
+			}
+		}
+
+		public static void main(String[] args) throws SQLException {
+			connectToMysql();
+			getAllDataUsingPreparedStatemnt();
+			updateDataUsingPreparedStatement("8000",3);
+		}	
+	}
