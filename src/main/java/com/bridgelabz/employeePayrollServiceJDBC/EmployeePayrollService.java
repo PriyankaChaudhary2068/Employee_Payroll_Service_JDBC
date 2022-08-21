5c138e63b58f6a5b4ea2cda7057baed284d6842e
chutki12 <priyankachaudhary2068>
2022-08-21 20:21:27 +0530

    ADDED : Connected Database To Java Program Successfully

--- src/main/java/com/bridgelabz/employeePayrollServiceJDBC/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeePayrollServiceJDBC/EmployeePayrollService.java
@@ -1,11 +1,36 @@
 package com.bridgelabz.employeePayrollServiceJDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class EmployeePayrollService {
 
-	public static void main(String[] args) {
-		
-		 System.out.println(" Welcome To Employee Payroll Service");
+	   public static void main(String[] args) throws SQLException {
+	        System.out.println("welcome to employee payroll data base connection ");
 
+	        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
+	        String userName = "root";
+	        String password = "vchutki2068vpc";
+	        Connection connection = null;
+
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            System.out.println("Driver loaded");
+	            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+
+	        } 
+	        catch (Exception e) 
+	        {
+	            System.out.println(e);
+	        } 
+	        finally 
+	        {
+	            connection.close();
+	        }
+	    }
 	}
+	
 
-}
+
